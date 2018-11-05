df67f9507753f3b58d3f3a934c39647af50e40eb
Fabio Waljaard <fabio.waljaard@gmail.com>
2018-11-05 11:16:16 +0100

    Read map from file

--- kmint_handout/kmintapp/src/main.cpp
+++ kmint_handout/kmintapp/src/main.cpp
@@ -3,6 +3,9 @@
 #include "kmint/math/vector2d.hpp" // voor window en app
 #include "kmint/play.hpp"          // voor stage
 #include "kmint/ui.hpp"            // voor window en app
+#include "kmint/map/map.hpp"		
+#include <iostream>
+#include <fstream>
 
 using namespace kmint; // alles van libkmint bevindt zich in deze namespace
 
@@ -12,9 +15,19 @@ int main() {
 
   //  maak een venster aan
   ui::window window{app.create_window({1024, 768}, "hello")};
+  auto mapStream = std::ifstream{ "resources/kaart.txt" };
+  map::map map{ map::read_map(mapStream) };
 
   // maak een podium aan
   play::stage s{};
+
+  // lees de kaart in
+  s.build_actor<play::background>(
+      math::size(1024, 768),
+      graphics::image{ map.background_image() });
+  s.build_actor<play::map_actor>(
+      math::vector2d{ 0.0f, 0.0f },
+      map.graph());
 
   // Maak een event_source aan (hieruit kun je alle events halen, zoals
   // toetsaanslagen)
